251b46fe6c08699b080da02987783ab9f19fbb98
Robert Laskowski <r.laskowski@poczta.fm>
2023-12-19 21:45:30 +0100

    Initial commit

---
+++ src/test/java/com/example/demo/PublisherMq.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PublisherMq {
+@Autowired
+    private RabbitTemplate rabbitTemplate;
+@GetMapping
+    public String get(@RequestParam String message, Object routingKey) {
+
+    rabbitTemplate.convertAndSend("message", message );
+    return "send";
+    }
+}
